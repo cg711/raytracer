4821dba32a39b72e5f9084011bce32e0f8cae116
cg711 <caseyg08@icloud.com>
2024-10-03 17:24:07 -0500

    antialiasing

--- color.h
+++ color.h
@@ -2,6 +2,7 @@
 #define COLOR_H
 
 #include "vec3.h"
+#include "interval.h"
 
 // Color represented by 3 element 256 valued vector
 using color = vec3;
@@ -12,9 +13,10 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto b = pixel_color.z();
 
     // Scale to [0,255]
-    int rb = int(255.999 * r);
-    int gb = int(255.999 * g);
-    int bb = int(255.999 * b);
+    static const interval intensity(0.000, 0.999);
+    int rb = int(256 * intensity.clamp(r));
+    int gb = int(256 * intensity.clamp(g));
+    int bb = int(256 * intensity.clamp(b));
 
     // Write pixel components
     out << rb << ' ' << gb << ' ' << bb << '\n';
